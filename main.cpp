82593af6aa1ddc859e8c9fd905e2a9104a5d248d
Нечта Богдан <bogdn05@mail.ru>
2025-06-10 15:05:58 +0300

    Реализована и подключена система работы с локалью через std::setlocale и chcp для Windows.

--- main.cpp
+++ main.cpp
@@ -39,7 +39,7 @@ int main(int argc, char* argv[]) {
             << "  polygon.exe <in> <out>\n";
         return 1;
     }
-
+    // если argc==1 — остаются input.txt и
 
     // 1) Синтаксическое чтение
     FileParser parser;
