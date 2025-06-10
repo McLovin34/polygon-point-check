d385ba44e329c537693ec4a996cc797fa42af436
Нечта Богдан <bogdn05@mail.ru>
2025-06-10 18:50:27 +0300

    Описание: Запись результата в файл В классе IOManager добавлена функция writeResult, которая открывает файл для записи результата. Функция записывает в файл строку, которая зависит от значения переменной result:
    Если result равен true, записывается строка "принадлежит".
    
    Если result равен false, записывается строка "не принадлежит".
    
    Обработка ошибок при открытии файла
    Если файл не удаётся открыть (например, из-за отсутствия прав на запись или неправильного пути), в объект err записывается тип ошибки outputFileCreateFail и подробное сообщение о проблеме с доступом к файлу.
    
    Закрытие файла
    После записи результата файл закрывается с помощью метода fout.close().
    
    Причина изменений:
    Данная функция была добавлена для сохранения результатов работы программы в файл и обеспечения обработки ошибок при попытке записи в файл.

--- IOManager.cpp
+++ IOManager.cpp
@@ -3,21 +3,26 @@
 #include <iostream>
 
 bool IOManager::writeResult(const std::string& fileName, bool result, Error& err) {
-    std::ofstream fout(fileName);
-    err.errorOutputFileWay = fileName;
+    std::ofstream fout(fileName);  // Открываем файл для записи
+    err.errorOutputFileWay = fileName;  // Указываем путь к файлу в объекте ошибки
+
+    // Проверяем, был ли файл успешно открыт
     if (!fout.is_open()) {
-        err.type = ErrorType::outputFileCreateFail;
-        err.errorMessage = "Неверно указан файл для выходных данных. Возможно, указанного расположения не существует или нет прав на запись.";
-        return false;
+        err.type = ErrorType::outputFileCreateFail;  // Ошибка, если файл не открылся
+        err.errorMessage = "Неверно указан файл для выходных данных. Возможно, указанного расположения не существует или нет прав на запись.";  // Сообщение об ошибке
+        return false;  // Возвращаем false, если файл не открылся
     }
+
+    // Записываем результат в файл в зависимости от значения переменной result
     if (result) {
-        fout << "принадлежит\n";
+        fout << "принадлежит\n";  // Если результат true, записываем "принадлежит"
     }
     else {
-        fout << "не принадлежит\n";
+        fout << "не принадлежит\n";  // Если результат false, записываем "не принадлежит"
     }
-    fout.close();
-    return true;
+
+    fout.close();  // Закрываем файл после записи
+    return true;  // Возвращаем true, если всё прошло успешно
 }
 
 void IOManager::writeErrorToConsole(const Error& err) {
